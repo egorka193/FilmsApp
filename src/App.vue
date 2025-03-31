efe0b1721735f0dd2e036ffd6af53b6078863a75
Egor Kondratev <kondratev.egor.2002@gmail.com>
2025-03-31 17:53:58 +0200

    Add primeVue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
   <div class="app">
     <p>App</p>
+    <InputText
+      v-model="value"
+      type="text" 
+    />
+    <Button>jr ksd</Button>
   </div>
 </template>
 
 <script lang="ts">
-export default {};
+import Button from "primevue/button";
+import InputText from 'primevue/inputtext';
+import { ref } from "vue";
+export default {
+  components: {
+    // eslint-disable-next-line vue/no-reserved-component-names
+    Button,
+    InputText,
+  },
+  setup() {
+    const value = ref('');
+
+    return {
+      value,
+    };
+  },
+};
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import './assets/main.css';
 
 import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
 
 app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.my-app-dark',
+      cssLayer: false,
+    },
+  },
+});
 
 app.mount('#app');
