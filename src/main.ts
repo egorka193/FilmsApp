4784f418b4bc873eb2d6f0b2c9bd33729dbd8f40
Egor Kondratev <kondratev.egor.2002@gmail.com>
2025-04-06 13:49:17 +0200

    Add Vuex

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
 import App from './App.vue';
 import router from './router';
-
+import { store, key } from './components/store/store.ts';
 
 const app = createApp(App);
+app.use(store, key);
 app.use(router);
 app.use(PrimeVue, {
   theme: {
